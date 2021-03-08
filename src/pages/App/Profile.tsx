4441ac7d2b42dfc4757b7a6e28d5764ea4fd8691
Matheus Faggi <matheusfaggi@gmail.com>
2021-03-07 21:04:51 -0300

    finish first scope

--- src/pages/App/Profile.tsx
+++ src/pages/App/Profile.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useContext } from 'react'
+import { View, Image } from 'react-native'
+import { Button, Headline } from 'react-native-paper'
+import { AuthContext } from '../../contexts'
+import ProfilePic from '../../assets/profile.jpeg'
 
 const Profile: React.FC = () => {
+  const { SignOut } = useContext(AuthContext)
   return (
-    <View>
-      <Text>Profile</Text>
+    <View
+      style={{
+        paddingHorizontal: 16,
+        paddingVertical: 32,
+        alignItems: 'center',
+      }}>
+      <Image
+        source={ProfilePic}
+        style={{ width: 100, height: 100, borderRadius: 100 }}
+      />
+      <Headline>Nome do usuário</Headline>
+      <Button onPress={() => SignOut()} mode="contained">
+        Sair
+      </Button>
     </View>
   )
 }
